dbd4a3a6c140265324826be5c5b7af19e766b167
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-04 17:22:15 +0800

    start parsing something

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Text (Text)
+import Data.Void
+
+type Parser = Parsec Void Text
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = parseTest (string "foo" :: Parser Text) "foo"
