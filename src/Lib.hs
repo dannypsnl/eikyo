cc8702f4b36e8621036d35ca6a6940729cf770ea
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-04 17:55:50 +0800

    initial data type parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+-- {-# LANGUAGE OverloadedRecordDot, OverloadedRecordUpdate, RebindableSyntax #-}
 module Lib
     ( someFunc
     ) where
-import Text.Megaparsec
+import Control.Monad
+import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import Data.Text (Text)
 import Data.Void
+import qualified Data.Text as T
+import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
 
 someFunc :: IO ()
-someFunc = parseTest (string "foo" :: Parser Text) "foo"
+someFunc = parseTest pDataType "type foo"
+
+data DataType = DataType
+    { name :: Text
+    -- , constructors :: [Field]
+    } deriving (Eq, Show)
+
+pDataType :: Parser DataType
+pDataType = do
+    void (string "type")
+    void space
+    name <- T.pack <$> some alphaNumChar
+    return $ DataType{..}
