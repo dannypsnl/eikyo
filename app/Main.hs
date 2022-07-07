788fb3d9b24f0d5a9ddf95a9ccded5ea1a07cae7
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-07 13:07:49 +0800

    setup command line option/arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
-import Lib
+import System.Console.CmdArgs
+import System.Console.CmdArgs.Explicit (HelpFormat (..), helpText, process)
+
+data Eikyo
+  = Compile {file :: FilePath}
+  | Build
+  deriving (Data, Typeable, Show, Eq)
+
+compile = Compile {file = def &= typ "FILE" &= argPos 0} &= help "Compile the file"
+
+build = Build &= help "(Not yet done) Build the project"
+
+mode =
+  cmdArgsMode $
+    modes [compile, build]
+      &= help "Eikyo compiler"
+      &= program "eikyo"
+      &= summary "Eikyo v0.0\n"
 
 main :: IO ()
-main = someFunc
+main = do
+  eikyo_mode <- cmdArgsRun mode
+  case eikyo_mode of
+    Compile {file = f} -> do
+      putStrLn $ "Compiling " ++ f ++ "..."
+    Build -> do
+      putStrLn "Building"
