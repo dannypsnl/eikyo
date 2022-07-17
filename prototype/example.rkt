a175b6d23430a5fd146404d119474d640db25eda
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-07-17 10:08:29 +0800

    initial mark affect implementation

--- prototype/example.rkt
+++ prototype/example.rkt
@@ -3,5 +3,8 @@
 (define x : number 3)
 (+ x x)
 
-(define a : (with-mark (+owned) number) 2)
-(+ a a)
+(define a : (with-mark (+owned test) number) 2)
+; FIXME: type mismatch: expected number, given (with-mark (owned) number)
+; this is because a has mark now
+; next thing is let type relation ignore the mark when no require
+; (+ a a)
--- prototype/info.rkt
+++ prototype/info.rkt
@@ -1,7 +1,8 @@
 #lang info
 (define collection "eikyo")
 (define deps '("base"
-               "turnstile"))
+               "turnstile"
+               "syntax-extension"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
